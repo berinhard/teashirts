d4d74e18caaee40743f7a30490a2eff94fe6bc46
Pedro Menezes <pedrojudo@gmail.com>
2012-05-24 16:05:54 -0300

    Adiciona espaço em branco no app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,6 @@ get "/" do
   file_path = "http://dl.dropbox.com/u/79616445/camiseteria.json"
   content = open(file_path).read()
   tshirts = JSON::parse(content)
+
   erb :index, :locals => {:tshirts => tshirts.first(5)}
 end
