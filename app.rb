052010c28a727753310d4f769f40f71b3ca0f03f
Pedro Menezes <pedrojudo@gmail.com>
2012-05-25 14:26:53 -0300

    adiciona página do camiseteria e parâmetro pra pegar somente N camisetas

--- app.rb
+++ app.rb
@@ -2,9 +2,17 @@ require "./settings"
 
 
 get "/" do
-  tshirts = CamiseteriaCatalog.new.shirts()
+  tshirts = CamiseteriaCatalog.new.shirts(5)
 
   erb :index, :locals => {
-    :tshirts => tshirts.first(5)
+    :tshirts => tshirts
+  }
+end
+
+get "/camiseteria" do
+  tshirts = CamiseteriaCatalog.new.shirts()
+
+  erb :'store/camiseteria', :locals => {
+    :tshirts => tshirts
   }
 end
--- test/unit/camiseteria_catalog_test.rb
+++ test/unit/camiseteria_catalog_test.rb
@@ -7,9 +7,11 @@ require './lib/camiseteria_catalog'
 
 
 describe CamiseteriaCatalog do
-  it "run and returns shirts" do
+  before do
     stub_camiseteria_request
+  end
 
+  it "returns all shirts" do
     shirts = CamiseteriaCatalog.new.shirts()
 
     shirts.size.must_equal(93)
@@ -20,11 +22,17 @@ describe CamiseteriaCatalog do
       "name" => "Joker"
     })
   end
+
+  it "returns the first N shirts" do
+    shirts = CamiseteriaCatalog.new.shirts(5)
+
+    shirts.size.must_equal(5)
+  end
 end
 
 
 def stub_camiseteria_request
-  File.open(File.expand_path(File.dirname(__FILE__) + '/helper/camiseteria.json'), 'r') do |f|
+  File.open(File.expand_path(File.dirname(__FILE__) + '/../helper/camiseteria.json'), 'r') do |f|
       stub_request(:get, "http://dl.dropbox.com/u/79616445/camiseteria.json").
           to_return(:body => f)
   end
