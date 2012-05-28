2db682e4f2412aa6c8bbc4f678f5ae2956d8eadc
Bernardo Fontes <falecomigo@bernardofontes.net>
2012-05-28 00:18:54 -0300

    Cria classe para fazer interface com o catálogo do Chico Rei

--- lib/camiseteria_catalog.rb
+++ lib/camiseteria_catalog.rb
@@ -17,3 +17,15 @@ class CamiseteriaCatalog
     results
   end
 end
+
+class ChicoReiCatalog
+  @@catalog_url = 'http://dl.dropbox.com/u/79616445/chico_rei.json'
+
+  def shirts(limit=nil)
+    content = open(@@catalog_url).read()
+    results = JSON::parse(content)
+
+    return results.first(limit) if limit
+    results
+  end
+end
